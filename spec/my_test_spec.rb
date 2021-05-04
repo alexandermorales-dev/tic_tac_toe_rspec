f4b9253141bd1d10d3b97da861fed741f90265e8
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-04 14:33:36 -0500

    add test for turn method

--- spec/my_test_spec.rb
+++ spec/my_test_spec.rb
@@ -4,11 +4,19 @@ require './lib/rules'
 
 describe Player do
   player1 = Player.new(name = 'alex', marker = 'X')
-  it "returns player name" do
+  it 'returns player name' do
     expect(player1.name).to eql('alex')
   end
 
-  it "returns player marker" do
+  it 'returns player marker' do
     expect(player1.marker).to eql('X')
+  end
+end
+
+describe PlayerT do
+  describe '#turn' do
+    it 'returns player turn' do
+      expect(PlayerT.turn(1, 'player 1 turn', 'player 2 turn')).to eql('player 1 turn')
+    end
   end
 end
