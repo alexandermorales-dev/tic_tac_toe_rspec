60160829c712ba8660c17983280f77b5d9728177
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-04 15:25:35 -0500

    add rspect test file

---
+++ spec/my_test_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require './lib/board'
+require './lib/player'
+require './lib/rules'
+
+describe 'Board' do
+  describe '#check' do
+    it "returns array with player's marker" do
+      expect(Board.check([[1, 2, 3]], 2, 'X')).to eql [[1, 'X', 3]]
+    end
+  end
+end
+
+describe 'Player' do
+  player1 = Player.new('alex', 'X')
+  it 'returns player name' do
+    expect(player1.name).to eql('alex')
+  end
+
+  it 'returns player marker' do
+    expect(player1.marker).to eql('X')
+  end
+end
+
+describe 'PlayerT' do
+  describe '#turn' do
+    it 'returns player turn' do
+      expect(PlayerT.turn(1, 'player 1 turn', 'player 2 turn')).to eql('player 1 turn')
+    end
+  end
+end
+
+describe 'Rules' do
+  describe '#win' do
+    array_winner = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    it "returns true if there's a winner" do
+      str1 = 'Player 1 you Win the Game'
+      str2 = 'Player 2 you win the game'
+      expect(Rules.win([1, 2, 3], [4, 5, 8], array_winner, str1, str2)).to be true
+    end
+  end
+end
